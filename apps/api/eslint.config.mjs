2d2b338303ff28995d84e9fd686d83cb6e8bdc81
galaxy1364 <galaxy.mehdi1364@gmail.com>
2025-12-28 17:50:50 +0330

    chore(lint): add eslint flat config for api

---
+++ apps/api/eslint.config.mjs
@@ -0,0 +1,25 @@
+﻿import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["**/*.ts"],
+    ignores: ["dist/**", "node_modules/**"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: "./tsconfig.json",
+        tsconfigRootDir: new URL(".", import.meta.url).pathname,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "no-console": "off",
+      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+    },
+  },
+];
