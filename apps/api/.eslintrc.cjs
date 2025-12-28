11dbbfcb3224b4cb21e1ce53523d3de7a2e8871d
galaxy1364 <galaxy.mehdi1364@gmail.com>
2025-12-28 18:06:04 +0330

    fix(lint): enable typescript-eslint parser for api

--- apps/api/.eslintrc.cjs
+++ apps/api/.eslintrc.cjs
@@ -1,6 +1,17 @@
 ﻿module.exports = {
   root: true,
   env: { node: true, es2022: true },
-  ignorePatterns: ["dist/", "node_modules/", "src/**/*.js"],
-  extends: ["eslint:recommended"],
+  ignorePatterns: ["dist/", "node_modules/"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    // Nest uses decorators; enable syntax
+    ecmaFeatures: { legacyDecorators: true }
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ]
 };
